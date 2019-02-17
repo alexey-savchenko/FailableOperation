887464ffa264daf67fe8b8b1a4cfda8f21f1d37a
Alexey Savchenko <nahalem@ukr.net>
2019-02-17 13:21:13 +0200

    Moved to structs from classes

--- FailableOperation/FailableOperation.swift
+++ FailableOperation/FailableOperation.swift
@@ -31,51 +31,63 @@ enum Result<T> {
   }
 }
 
-class FailableSyncOperation<Input, Output> {
+struct FailableSyncOperation<Input, Output> {
+  
   typealias ResultHandler = (Result<Output>) -> Void
   typealias SyncOperation = (Input) -> Result<Output>
-  private var attempts = 0
-  private var maxAttempts: Int
-  private let operation: SyncOperation
   
-  init(_ maxAttempts: Int = 3, operation: @escaping SyncOperation) {
+  private var attempts = 0
+  private let maxAttempts: Int
+  private let wrapped: SyncOperation
+  
+  init(_ maxAttempts: Int = 2, operation: @escaping SyncOperation) {
     self.maxAttempts = maxAttempts
-    self.operation = operation
+    self.wrapped = operation
   }
   
   func execute(with input: Input, completion: ResultHandler) {
-    attempts += 1
-    let result = operation(input)
+    let result = wrapped(input)
     if result.isFail && attempts < maxAttempts {
-      execute(with: input, completion: completion)
+      spawnOperation(with: attempts + 1).execute(with: input, completion: completion)
     } else {
       completion(result)
     }
   }
+  
+  private func spawnOperation(with attempts: Int) -> FailableSyncOperation<Input, Output> {
+    var op = FailableSyncOperation(maxAttempts, operation: wrapped)
+    op.attempts = attempts
+    return op
+  }
 }
 
-// TODO: Move to structs and avoid mutation of retries var via instantiation of new instance of operation with updated retries value
-
-class FailableAsyncOperation<Input, Output> {
+struct FailableAsyncOperation<Input, Output> {
+  
   typealias ResultHandler = (Result<Output>) -> Void
   typealias AsyncOperation = (Input, (Result<Output>) -> Void) -> Void
-  private var retries = 0
-  private var maxRetries: Int
-  private let operation: AsyncOperation
   
-  init(_ maxRetries: Int = 3, operation: @escaping AsyncOperation) {
-    self.maxRetries = maxRetries
-    self.operation = operation
+  private var attempts = 0
+  private let maxAttempts: Int
+  private let wrapped: AsyncOperation
+  
+  init(_ maxAttempts: Int = 2, operation: @escaping AsyncOperation) {
+    self.maxAttempts = maxAttempts
+    self.wrapped = operation
   }
   
   func execute(with input: Input, completion: ResultHandler) {
-    retries += 1
-    operation(input) { result in
-      if result.isFail && retries < maxRetries {
-        execute(with: input, completion: completion)
+    wrapped(input) { result in
+      if result.isFail && attempts < maxAttempts {
+        spawnOperation(with: attempts + 1).execute(with: input, completion: completion)
       } else {
         completion(result)
       }
     }
+  }
+  
+  private func spawnOperation(with attempts: Int) -> FailableAsyncOperation<Input, Output> {
+    var op = FailableAsyncOperation(maxAttempts, operation: wrapped)
+    op.attempts = attempts
+    return op
   }
 }
--- FailableOperation/ViewController.swift
+++ FailableOperation/ViewController.swift
@@ -13,6 +13,40 @@ class ViewController: UIViewController {
   override func viewDidLoad() {
     super.viewDidLoad()
     
+    struct SomeError: Error {
+      
+    }
+    
+    let sync = FailableSyncOperation<Void, Int> { _ in
+      if arc4random_uniform(10) < 5 {
+        print("Sync operation fail")
+        return Result.fail(SomeError())
+      } else {
+        print("Sync operation success")
+        return Result.success(42)
+      }
+    }
+    
+    sync.execute(with: ()) { (result) in
+      print("Result of failable sync operaion - \(result)")
+    }
+    
+    func someAsyncFunction(_ completion: ((Result<Int>) -> Void)) {
+      if arc4random_uniform(10) < 5 {
+        print("Async operation fail")
+        completion(.fail(SomeError()))
+      } else {
+        print("Async operation success")
+        completion(.success(42))
+      }
+    }
+    
+    let async = FailableAsyncOperation<Void, Int> { (input, handler) in
+      someAsyncFunction(handler)
+    }
+    
+    async.execute(with: ()) { (result) in
+      print("Result of failable async operaion - \(result)")
+    }
   }
-
 }
