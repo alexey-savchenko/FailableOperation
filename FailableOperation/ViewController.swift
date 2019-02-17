40ad788543aa5c55bac13fd5da7ef9a3bfb97a37
Alexey Savchenko <nahalem@ukr.net>
2019-02-17 12:58:55 +0200

    Initial Commit

---
+++ FailableOperation/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  FailableOperation
+//
+//  Created by Alexey Savchenko on 2/17/19.
+//  Copyright © 2019 Alexey Savchenko. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+
+}
+
